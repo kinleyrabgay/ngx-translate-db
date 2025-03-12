a48af112878dbb53cbdcd21fe55bf96cd279ac05
kinleyrabgay <fsd.rabgay@gmail.com>
2025-03-12 11:15:31 +0600

    app: provider on app intialization
    Signed-off-by: kinleyrabgay <fsd.rabgay@gmail.com>

--- src/lib/providers/translate.provider.ts
+++ src/lib/providers/translate.provider.ts
@@ -1,15 +1,25 @@
-import { Provider } from "@angular/core";
+import { APP_INITIALIZER, Provider } from "@angular/core";
 import { TranslateService } from "../services/translate.service";
 import { TranslationConfig } from "../interfaces/translation.interface";
 import { TranslatePipe } from "../pipes/translate.pipe";
+
+export function initializeTranslation(translateService: TranslateService, config: TranslationConfig) {
+  return () => translateService.init(config);
+}
 
 export function provideTranslate(config: TranslationConfig): Provider[] {
   return [
     TranslateService,
     TranslatePipe,
     {
-      provide: 'TRANSLATE_CONFIG',
-      useValue: config
-    }
+      provide: "TRANSLATE_CONFIG",
+      useValue: config,
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (translateService: TranslateService, config: TranslationConfig) => initializeTranslation(translateService, config),
+      deps: [TranslateService, "TRANSLATE_CONFIG"],
+      multi: true,
+    },
   ];
 }
