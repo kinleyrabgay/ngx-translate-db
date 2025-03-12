1d7edf6ddea9e4c62d010abe82920bdadf8c0bf6
kinleyrabgay <fsd.rabgay@gmail.com>
2025-03-12 11:21:50 +0600

    app: provider
    Signed-off-by: kinleyrabgay <fsd.rabgay@gmail.com>

--- src/lib/providers/translate.provider.ts
+++ src/lib/providers/translate.provider.ts
@@ -3,23 +3,18 @@ import { TranslateService } from "../services/translate.service";
 import { TranslationConfig } from "../interfaces/translation.interface";
 import { TranslatePipe } from "../pipes/translate.pipe";
 
-export function initializeTranslation(translateService: TranslateService, config: TranslationConfig) {
-  return () => translateService.init(config);
-}
-
 export function provideTranslate(config: TranslationConfig): Provider[] {
   return [
     TranslateService,
     TranslatePipe,
     {
-      provide: "TRANSLATE_CONFIG",
-      useValue: config,
-    },
-    {
       provide: APP_INITIALIZER,
-      useFactory: (translateService: TranslateService, config: TranslationConfig) => initializeTranslation(translateService, config),
-      deps: [TranslateService, "TRANSLATE_CONFIG"],
-      multi: true,
-    },
+      useFactory: (service: TranslateService) => {
+        console.log('initializeTranslation', service, config);
+        return () => service.init(config);
+      },
+      deps: [TranslateService],
+      multi: true
+    }
   ];
 }
