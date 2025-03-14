9f74f093c8324bdfab295b7d1e6e62a3f54ddcd8
kinleyrabgay <fsd.rabgay@gmail.com>
2025-03-14 12:12:37 +0600

    pkg: v17 cache
    Signed-off-by: kinleyrabgay <fsd.rabgay@gmail.com>

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-/// <amd-module name="ngx-translate-db" />
-export * from './public-api';
--- dist/public-api.d.ts
+++
@@ -1,4 +0,0 @@
-export { TranslateService } from "./lib/services/translate.service";
-export { TranslatePipe } from "./lib/pipes/translate.pipe";
-export { provideTranslate } from "./lib/providers/translate.provider";
-export { TranslationConfig, TranslationValue, TranslationLanguage, Translations, } from "./lib/interfaces/translation.interface";
